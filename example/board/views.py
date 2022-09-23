62e197e6b95a366c8a98e0ceebd2fd7c721d80c9
toto9602 <toto9602@naver.com>
2022-09-24 00:46:45 +0900

    feat : PostListAPIView에서 모든 Post를 조회하는 부분까지 작성

--- example/board/views.py
+++ example/board/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Post
+
+# APIView를 상속받아 CBV로 작성합니다.
+# APIView를 상속받은 클래스 안에 request method에 맞는 함수들을 정의해주면
+# 각각의 요청은 request method 이름에 맞게 구분되어 그에 맞는 결과를 반환합니다.
+class PostListAPIView(APIView):
+    # URL : board/posts 
+    # HTTP Method : GET
+    def get(self, request):
+        posts = Post.objects.all()
+
+
+
+
